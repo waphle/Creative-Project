57c5a7c07775ba0932100e8c3ecf98c95e171f51
Jeff Zhang <jz1766american.edu>
2020-03-24 13:40:37 -0400

    Updated.
    Added setTimer() for email on all HTML pages, fixed issue with transitions not working due to link the JS file.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Portrait stuff
+// Portrait url
 function myFunction() {
   var x = document.getElementById("portraitJeff").src;
   document.getElementById("demo").innerHTML = x;
+}
+
+// Timer alert message for email
+function emailFunction() {
+  setTimeout(function(){ alert('Left click the link and choose "Copy email address" to copy my email address to your clipboard.'); }, 2000); // 2000 equals to 2 seconds
 }
