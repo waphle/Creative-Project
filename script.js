fc6fed851f489cb59136f533e80fb1c86666c595
Jeff Zhang <jz1766american.edu>
2020-04-20 14:07:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,13 @@ fetch('./api/some.json')
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
+
+fetch('users.json').then(function(response) {
+  console.log(response.headers.get('Content-Type'));
+  console.log(response.headers.get('Date'));
+
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.type);
+  console.log(response.url);
+});
